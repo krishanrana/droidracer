c0aa8c85353c99a39dcacb2340e085f0f50496f0
Sam Armstrong <sam.armstrong88@gmail.com>
2018-06-30 21:25:26 +1000

    now waits until first frame arrives

--- example_picamerastream.py
+++ example_picamerastream.py
@@ -6,7 +6,7 @@ if __name__=="__main__":
     droid = piBotServer.PiBotServer()
     droid.StartCameraStream()           # Make sure you start the camera stream!
     while True:
-        if not droid.cam.frame_available:   # Wait till a frame read to the buffer
+        while not droid.cam.frame_available:   # Wait till a frame read to the buffer
             time.sleep(0.01)
         droid.cam.frame_available = False   # Reset the flag so you aren't processing the same image over and over
         im = droid.cam.read()
