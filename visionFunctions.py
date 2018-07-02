11837546895f9c11b269de6a1bfa6fcc34aa1556
Sam Armstrong <sam.armstrong88@gmail.com>
2018-07-02 10:45:25 +1000

    put good vision code into class

--- visionFunctions.py
+++ visionFunctions.py
@@ -1,207 +1,167 @@
 import cv2
 import numpy as np
 
-
+from collections import deque
 
 class droidVision():
 
     def __init__(self):
-        pass
+
+        self.dataAvailable = 0
+        self.failedFrame = 0
+        self.histVPHeading = deque([0],10)
+        self.histLeftOffset = deque([0],10)
+        self.histRightOffset = deque([0],10)
+        self.histObDist = deque([0],10)
+        self.obMissing = 0
+
+        self.frameNo = 0
+        self.kernel = np.ones((5,5),np.uint8)
+
+        self.vpX = 0
+        self.vpY = 0
+        self.leftOffset = 0
+        self.rightOffset = 0
+
+        self.clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        self.thetaThresh = np.pi/180 *1.5
+        self.rhoThresh = 80
+        self.minLineLength = 100
+        self.maxLineGap = 30
+        self.centreY = 400 # Result of calibration
 
 
     def processFrame(self, frame):
-        frame = frame[450:frame.shape[0]-1,:,:]
-
-        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-        thetaThresh = np.pi/180 *1.5
-        rhoThresh = 80
-        minLineLength = 100
-        maxLineGap = 30
-        kernel = np.ones((5,5),np.uint8)
         
-        # Calibrate frame centre and add pixel/m map
-        centreY = 400 # Result of calibration
-        mPerPixel = 0.004
-        
-        # Initialise variables
         width = frame.shape[1]
-        dataAvailable = 1; 
-        vpHeading = 0
-        obCentre = [0,0]
-        obDistance = 0
-        obHeading = 0
-        vpX = 0
-        vpY = 0
-        leftOffset = 0
-        rightOffset = 0
 
-        #State flags
-        blueDetect = 0
-        yellowDetect = 0
-        obstacle = 0
         
-        # Work with frame
-        centreX = np.round(width/2)
+        centreX = np.round(frame.shape[1]/2)
         processed = cv2.medianBlur(frame,5)
-        lab = cv2.cvtColor(processed,cv2.COLOR_BGR2LAB) 
-        l,a,b = cv2.split(lab)
-
-        # histogram adjustment
-        clA = clahe.apply(a) 
-        clB = clahe.apply(b) 
+        processed = cv2.cvtColor(processed,cv2.COLOR_BGR2LAB) 
+        l,a,b = cv2.split(processed)
+        clA = self.clahe.apply(a) # histogram adjustment
+        # Split into yellow and blue lines
+        thresh,purple = cv2.threshold(clA,160,1,cv2.THRESH_BINARY)
         
-        # Split into yellow and blue and green lines
+        clB = self.clahe.apply(b) # histogram adjustment
+        # Split into yellow and blue lines
         retY,yellow = cv2.threshold(clB,170,1,cv2.THRESH_BINARY)
         retB,blue = cv2.threshold(clB,100,1,cv2.THRESH_BINARY)
-#        retG,green = cv2.threshold(clA,100,1,cv2.THRESH_BINARY)
-        retP,purple = cv2.threshold(clA,160,1,cv2.THRESH_BINARY)
-        
-#        green = cv2.bitwise_not(green) - 254 # invert green line
-        blue = cv2.bitwise_not(blue) - 254 # invert blue line
+        blue = cv2.bitwise_not(blue)-254 # invert blue line
         try:
             # process yellow line
-            yellow = cv2.morphologyEx(yellow, cv2.MORPH_OPEN, kernel)
-            yLine = np.squeeze(cv2.HoughLinesP(yellow,1,thetaThresh,rhoThresh,minLineLength,maxLineGap))
-            yGrad = (yLine[:,0]-yLine[:,2])/(yLine[:,1]-yLine[:,3] + 0.001)
-            yFilt = rejectOutliers(yGrad, m=10)
-            
-            yMag = np.sqrt((yLine[:,1]-yLine[:,3])**2+(yLine[:,0]-yLine[:,2])**2) 
-            yM = np.sum((yFilt*yMag),axis=0)/np.sum(yMag,axis=0) 
-            # find intersection point with baseline centreY, using gradient and mean point
-            yX,yY = np.sum((yLine[:,0] + yLine[:,2])/(2*yLine.shape[0])),np.sum((yLine[:,1] + yLine[:,3])/(2*yLine.shape[0]))
-            yZero = yX + yM*(centreY-yY)
-            yellowDetect = 1
-            for x1,y1,x2,y2 in yLine: 
-                cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),1)
+            yellow = cv2.morphologyEx(yellow, cv2.MORPH_OPEN, self.kernel)
+            #yellow = cv2.Canny(yellow,0,1,apertureSize = 5)
+            yline = np.squeeze(cv2.HoughLinesP(yellow,1,self.thetaThresh,self.rhoThresh,self.minLineLength,self.maxLineGap))#detect lines
+            ygrad = (yline[:,0]-yline[:,2])/(yline[:,1]-yline[:,3]+0.001)# find gradient of lines
+            yfilt = rejectOutliers(ygrad, m=10)
+            ymag = np.sqrt((yline[:,1]-yline[:,3])**2+(yline[:,0]-yline[:,2])**2) # find magnitude of lines
+            yM = np.sum((yfilt*ymag),axis=0)/np.sum(ymag,axis=0) # find weighted average gradient
+            #find intersection point with baseline centreY, using gradient and mean point
+            ypointX,ypointY = np.sum((yline[:,0] + yline[:,2])/(2*yline.shape[0])),np.sum((yline[:,1] + yline[:,3])/(2*yline.shape[0]))
+            yZeroCrossing = ypointX + yM*(self.centreY-ypointY)
+            #for x1,y1,x2,y2 in yline: 
+            #cv2.line(frame,(x1,y1),(x2,y2),(0,0,255),1)
         except:
-            yellowDetect = 0
+            print('No yellow') 
             
-            
-        # Detect blue line    
         try:   
             # process blue line
-            blue = cv2.morphologyEx(blue, cv2.MORPH_OPEN, kernel)
-            bLine = np.squeeze(cv2.HoughLinesP(blue,1,thetaThresh,rhoThresh,minLineLength,maxLineGap))
-            bGrad = (bLine[:,0]-bLine[:,2])/(bLine[:,1]-bLine[:,3] + 0.001)
-            bFilt = rejectOutliers(bGrad,m=10)
+            blue = cv2.morphologyEx(blue, cv2.MORPH_OPEN, self.kernel)
+    #        blue = cv2.Canny(blue,0,1,apertureSize = 5)
+            bline = np.squeeze(cv2.HoughLinesP(blue,1,self.thetaThresh,self.rhoThresh,self.minLineLength,self.maxLineGap))#detect lines
+            bgrad = (bline[:,0]-bline[:,2])/(bline[:,1]-bline[:,3])# find gradient of lines
+            bfilt = rejectOutliers(bgrad, m=10)
 
-            bMag = np.sqrt((bLine[:,1]-bLine[:,3])**2+(bLine[:,0]-bLine[:,2])**2) 
-            bM = np.sum((bFilt*bMag),axis=0)/np.sum(bMag,axis=0) 
-            
+            bmag = np.sqrt((bline[:,1]-bline[:,3])**2+(bline[:,0]-bline[:,2])**2) # find magnitude of lines
+            bM = np.sum((bfilt*bmag),axis=0)/np.sum(bmag,axis=0) # find weighted average gradient
             #find intersection point with baseline centreY, using gradient and mean point
-            bX,bY = np.sum((bLine[:,0] + bLine[:,2])/(2*bLine.shape[0])),np.sum((bLine[:,1] + bLine[:,3])/(2*bLine.shape[0]))
-            bZero = bX + bM * (centreY-bY)
-            blueDetect = 1
-            for x1,y1,x2,y2 in bLine:
-                cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),1)
-
+            bpointX,bpointY = np.sum((bline[:,0] + bline[:,2])/(2*bline.shape[0])),np.sum((bline[:,1] + bline[:,3])/(2*bline.shape[0]))
+            bZeroCrossing = bpointX + bM*(self.centreY-bpointY)
+        
+    #        for x1,y1,x2,y2 in bline:
+    #            cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),1)
         except:
-            blueDetect = 0
-        # Detect finish line - Unfinished, need example
-#        try:   
-#            # process green line
-#            green = cv2.morphologyEx(green, cv2.MORPH_OPEN, kernel)
-#            gline = np.squeeze(cv2.HoughLinesP(green,1,thetaThresh,rhoThresh,minLineLength,maxLineGap))#detect lines
-#            ggrad = (gline[:,0]-gline[:,2])/(gline[:,1]-gline[:,3])# find gradient of lines
-#            gmag = np.sqrt((gline[:,1]-gline[:,3])**2+(gline[:,0]-gline[:,2])**2) # find magnitude of lines
-#            gM = np.sum((ggrad*gmag),axis=0)/np.sum(gmag,axis=0) # find weighted average gradient
-#                      
-#
-#        except:
-#            print('No green') 
+            print('No blue')
             
-        # Detect obstacles
         try:
             # process purple objects
-            purple = cv2.morphologyEx(purple, cv2.MORPH_OPEN, kernel)
+            purple = cv2.morphologyEx(purple, cv2.MORPH_OPEN, self.kernel)
+            # blob detect,get centroids
             __, contours, __ = cv2.findContours(purple,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
             # find centroid of largest blob
+        
             blob = max(contours, key=lambda el: cv2.contourArea(el))
             M = cv2.moments(blob)
-            obCentre = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
             
             # Find edges of obstacle
-            blob = blob
+            cnt = blob
 
-            obLeft = tuple(blob[blob[:,:,0].argmin()][0])
-            obRight = tuple(blob[blob[:,:,0].argmax()][0])
-            #obTop = tuple(blob[blob[:,:,1].argmin()][0])
-            obBottom = tuple(blob[blob[:,:,1].argmax()][0])
-            
+            leftEdge = tuple(cnt[cnt[:,:,0].argmin()][0])
+            rightEdge = tuple(cnt[cnt[:,:,0].argmax()][0])
+            topEdge = tuple(cnt[cnt[:,:,1].argmin()][0])
+            bottomEdge = tuple(cnt[cnt[:,:,1].argmax()][0])
             # Calculate distance to object
-            obDistance = (centreY - obBottom[1]) * mPerPixel
-            obHeading = np.arctan((obCentre[0] - centreX)/(centreY - obCentre[1]))
-            print('Obstacle detected')
+            obDistance = (self.centreY - bottomEdge[1])*0.04
+
+            # Draw outputs
+            cv2.rectangle(frame,(0,bottomEdge[1]),(width,bottomEdge[1]),(0,0,255),2)
+            cv2.circle(frame, center, 5, (0,0,255), -1)
             obstacle = 1
             
-
         except:
             print('No objects, drive fast!') 
             obstacle = 0
             
-            
-        linestate = yellowDetect + blueDetect
+
         
-        try:   
-            if linestate == 0:
-                dataAvailable = 0
-                print('No lines') 
+        try:
+            leftOffset = (centreX-bZeroCrossing)
+            rightOffset = (yZeroCrossing - centreX)
+            centreOffset = rightOffset-leftOffset
+            self.vpY = (yZeroCrossing - bZeroCrossing)/(bM - yM) + self.centreY
+            self.vpX = bM * (self.vpY - self.centreY) + bZeroCrossing
+            Heading = np.arctan((self.vpX - centreX)/(self.centreY - self.vpY))
+            dataAvailable = 1
 
-            
-                
-            elif linestate == 1:
-                dataAvailable = 1
-                # In case of occlusion
-#                if obstacleDetect:
-                    
-                if blueDetect:
-                    leftOffset = (centreX-bZero)
-                    rightOffset = np.NaN
-                    vpY = 0
-                    vpX = (bM * centreY) + centreX
-                    vpHeading = np.arctan((vpX - centreX)/(centreY - vpY))
-                    print('Just blue ')
-
-                elif yellowDetect:
-                    leftOffset =  np.NaN
-                    rightOffset = (centreX-yZero)
-                    vpY = 0
-                    vpX = (yM * centreY) + centreX
-                    vpHeading = np.arctan((vpX - centreX)/(centreY - vpY))
-                    print('Just yellow ')
-#                else:
-#                    dataAvailable = 1
-#                    print('1 line, no obstacle')
-                        
-                
-                
-            else:
-                # Situation normal, 2 lines seen
-                dataAvailable = 1
-                leftOffset = (centreX-bZero)
-                rightOffset = (yZero - centreX)
-                vpY = (yZero - bZero)/(bM - yM) + centreY
-                vpX = bM * (vpY - centreY) + bZero
-                vpHeading = np.arctan((vpX - centreX)/(centreY - vpY))
-                print('2 lines')
- 
+    
         except:
             print('all fucked right now')
-            dataAvailable = 0;
-        try:
-            cv2.line(frame,(int(bZero),int(centreY)),(int(vpX),int(vpY)),(0,255,0),2)
-            cv2.line(frame,(int(yZero),int(centreY)),(int(vpX),int(vpY)),(0,255,0),2)
-        except:
-            print('cant show lines')  
+            self.dataAvailable = 0
             
-        try:
-            cv2.line(frame,(0,obBottom[1]),(width,obBottom[1]),(0,255,0),2)
-            cv2.circle(frame, obCentre, 5, (0,255,0), -1)
+        if dataAvailable:   
+            self.histVPHeading.append(Heading)
+            self.histLeftOffset.append(leftOffset)
+            self.histRightOffset.append(rightOffset)
+        else:
+            self.failedFrame += 1
+            # If lines are not detected for 10 frames, remove history
+        if self.failedFrame >= 10:
+            self.histVPHeading = deque([0],10)
+            self.histLeftOffset = deque([0],10)
+            self.histRightOffset = deque([0],10)
+            print('10 failed frames')
         
-        except:
-            print('cant show obstacle')
+        if obstacle:
+            self.obMissing = 0
+            self.histObDist.append(obDistance)
             
-        return dataAvailable,vpHeading, leftOffset, rightOffset, obstacle, obDistance, obHeading
+        else:
+            self.obMissing +=1
+            
+        if self.obMissing >= 10:
+            self.histObDist = deque([0],10)
+            print('Lost the obstacle')
+                
+            
+        avHeading = np.nanmedian(self.histVPHeading)
+        avLeftOffset = np.nanmedian(self.histLeftOffset)
+        avRightOffset = np.nanmedian(self.histRightOffset)
+        avObDist = np.nanmedian(self.histObDist)
+            
+        return dataAvailable, avHeading, leftOffset, rightOffset, obstacle, obDistance #, obHeading
 
 
             
