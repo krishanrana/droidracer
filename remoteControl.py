c9609403f55b947258a3201945f68115e6286fcd
Sam Armstrong <sam.armstrong88@gmail.com>
2018-07-05 21:14:14 +1000

    Changed to pygame control. So much better! <3

--- remoteControl.py
+++ remoteControl.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 import numpy as np
+import pygame
 import logging
 logging.basicConfig(level=logging.INFO,
                       format='[%(levelname)s] (%(threadName)-9s) %(message)s',)
@@ -16,6 +17,11 @@ noPressCounter = 0
 
 if __name__=='__main__':
 
+    pygame.init()
+    screen = pygame.display.set_mode((100,50))
+
+    keys = pygame.key.get_pressed()
+
     if SAVE_VIDEO:
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
         size = (int(DEFAULT_CAM_W*CAM_SCALING),int(DEFAULT_CAM_H*CAM_SCALING))
@@ -30,7 +36,7 @@ if __name__=='__main__':
         time.sleep(0.01)
     key = cv2.waitKey(10)
 
-    while key != ord('q'):
+    while (not keys[pygame.K_q]): # Quit key 'q'
 
         # Grab the frame
         if droid.frame_available:
@@ -41,80 +47,78 @@ if __name__=='__main__':
                 out.write(im)
 
             cv2.imshow("Droid Vision", im)
+            cv2.waitKey(5)
         
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:                
+                keys = pygame.key.get_pressed()
+                '''
+                #####################################################################################
+                    DRIVING KEYS
+                #####################################################################################
+                '''        
 
-        # Get the next key press
-        key = cv2.waitKey(10)
+                if droid.state == 0:
 
-        '''
-        #####################################################################################
-            DRIVING KEYS
-        #####################################################################################
-        '''
-        if droid.state == 0:
-            if key == ord('w'): # Forward
-                logging.debug("Going forward")
-                droid.setSpeed(SPEED, 1.571, 0)
-                noPressCounter = 0
+                    if (keys[pygame.K_w] and keys[pygame.K_d]): # Forward right
+                        droid.setSpeed(SPEED, 1.571, -OMEGA)
 
-            elif key == ord('s'): # Back
-                logging.debug("Going back")
-                droid.setSpeed(SPEED, 4.712, 0)
-                noPressCounter = 0
+                    elif (keys[pygame.K_w] and keys[pygame.K_a]): # Forward left
+                        droid.setSpeed(SPEED, 1.571, OMEGA)
 
-            elif key == ord('a'): # Left
-                logging.debug("Going left")
-                droid.setSpeed(SPEED, 3.142, 0)
-                noPressCounter = 0
+                    elif (keys[pygame.K_s] and keys[pygame.K_d]): # Back right
+                        droid.setSpeed(-SPEED, 1.571, OMEGA)
 
-            elif key == ord('d'): # Right
-                logging.debug("Going right")
-                droid.setSpeed(SPEED, 0, 0)
-                noPressCounter = 0
-            
-            elif key == ord('z'): # Spin left
-                logging.debug("Spinning left")
-                droid.setSpeed(0, 0, OMEGA)
-                noPressCounter = 0
+                    elif (keys[pygame.K_s] and keys[pygame.K_a]): # Back Left
+                        droid.setSpeed(-SPEED, 1.571, -OMEGA)
 
-            elif key == ord('c'): # Spin right
-                logging.debug("Spinning right")
-                droid.setSpeed(0, 0, -OMEGA)
-                noPressCounter = 0
+                    elif (keys[pygame.K_w]): # Forward
+                        logging.debug("Going forward")
+                        droid.setSpeed(SPEED, 1.571, 0)
 
-            elif key == ord('x'): # Stop
-                logging.debug("Stop")
-                droid.setSpeed(0, 0, 0)
-                noPressCounter = 0
+                    elif (keys[pygame.K_s]): # Back
+                        logging.debug("Going back")
+                        droid.setSpeed(SPEED, 4.712, 0)   
+                                    
+                    elif (keys[pygame.K_d]): # Spin CW
+                        logging.debug("Spinning clockwise")
+                        droid.setSpeed(0, 0, -OMEGA)
+
+                    elif (keys[pygame.K_a]): # Spin CCW
+                        logging.debug("Spinning counterclockwise")
+                        droid.setSpeed(0, 0, OMEGA)
+
+                    elif (keys[pygame.K_LEFT]): # Strafe left
+                        logging.debug("Strafing left")
+                        droid.setSpeed(SPEED, 3.141, 0)
+
+                    elif (keys[pygame.K_RIGHT]): # Strafe right
+                        logging.debug("Strafing right")
+                        droid.setSpeed(SPEED, 0, 0)
+
+                    else:
+                        logging.debug("Stop")
+                        droid.setSpeed(0, 0, 0)
+                '''
+                #####################################################################################
+                    SETTING KEYS
+                #####################################################################################
+                '''
+                if (keys[pygame.K_m]): # Manual mode
+                    logging.info("Manual mode initialised")
+                    droid.setMode('manual')
+
+                elif (keys[pygame.K_k]): # Auto mode
+                    logging.info("Autonomous mode initialised")
+                    droid.setMode('auto')
                 
-            else:
-                noPressCounter += 1
-                if noPressCounter >= 10:
-                    droid.setSpeed(0, 0, 0)
-                    noPressCounter = 0
-
-        '''
-        #####################################################################################
-            SETTING KEYS
-        #####################################################################################
-        '''
-        if key == ord('m'): # Manual mode
-            logging.info("Manual mode initialised")
-            droid.setMode('manual')
-
-        elif key == ord('k'): # Auto mode
-            logging.info("Autonomous mode initialised")
-            droid.setMode('auto')
-        
-        elif key == ord('y'): # Take photo
-            logging.info("Taking photo")
-            cv2.imwrite('test_videos/image.jpg', im)
+                elif (keys[pygame.K_y]): # Take photo
+                    logging.info("Taking photo")
+                    cv2.imwrite('test_videos/image.jpg', im)
 
     droid.StopCamStream()
 
     if SAVE_VIDEO:
         out.release() # Finish the movie file off
 
-    logging.info("Press any key to exit.")
-    cv2.waitKey(0)
-
+    pygame.quit()
