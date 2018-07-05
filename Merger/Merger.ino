47d5e25f0be12f96bd0f0c841f1c251ae096e7ae
Sam Armstrong <sam.armstrong88@gmail.com>
2018-07-05 13:45:23 +1000

    undo wheel diameter thing

--- Merger/Merger.ino
+++ Merger/Merger.ino
@@ -297,9 +297,9 @@ void computeVelocities(float vel, float heading, float angular_vel) {
   // 3 wheel omniwheel kinematics
   // Transforms from velocity/heading/angular velocity to motor speeds
   
-  setspeed_M1 = (vel * (-0.5 * cos(heading) - sqrt(3) / 2 * sin(heading))/ WHEEL_DIAMETER) + (2 * angular_vel * DROIDRADIUS / WHEEL_DIAMETER);
-  setspeed_M2 = (vel * (-0.5 * cos(heading) + sqrt(3) / 2 * sin(heading))/ WHEEL_DIAMETER) + (2 * angular_vel * DROIDRADIUS / WHEEL_DIAMETER);
-  setspeed_M3 = (vel * cos(heading)/ WHEEL_DIAMETER) + (2 * angular_vel * DROIDRADIUS / WHEEL_DIAMETER);
+  setspeed_M1 = (vel * (-0.5 * cos(heading) - sqrt(3) / 2 * sin(heading))) + (2 * angular_vel * DROIDRADIUS);
+  setspeed_M2 = (vel * (-0.5 * cos(heading) + sqrt(3) / 2 * sin(heading))) + (2 * angular_vel * DROIDRADIUS);
+  setspeed_M3 = vel * cos(heading) + (2 * angular_vel * DROIDRADIUS);
 
 }
 
