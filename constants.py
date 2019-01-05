4588ee09aa1c13ea7d52be13acc2e50cd128fc27
doug palmer <douglas.palmer@connect.qut.edu.au>
2019-01-06 00:53:07 +1000

    constants returned

---
+++ constants.py
@@ -0,0 +1,17 @@
+# Communications settings
+DEFAULT_HOST = '192.168.0.6'
+
+PORT_CMDS_A = 5000
+PORT_CMDS_B = 5001
+PORT_CAMERA = 5002
+
+# Camera settings
+#DEFAULT_CAM_W = 1640
+#DEFAULT_CAM_H = 922
+DEFAULT_CAM_W = 416
+DEFAULT_CAM_H = 304
+
+CAM_SCALING = 1 # Scales the camera image to reduce processing overhead
+
+DEFAULT_CAM_TILT = 5.93 # MEASURED 20 DEGREES IN RAD
+DEFAULT_CAM_HEIGHT = 200 # TODO
