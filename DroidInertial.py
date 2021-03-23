21f6d1c37c0e91d74fef13205df501667ff1dbad
dougmatic2000 <douglas.palmer@connect.qut.edu.au>
2021-03-23 16:26:59 +1000

    DroidInertial updated; Calibration and visualisation functions added

--- DroidInertial.py
+++ DroidInertial.py
@@ -13,11 +13,11 @@ from MPUClass.MPU6050 import MPU6050 # Rewrite class using native numpy
 import logging
 import time
 import matplotlib.pyplot as plt
-import PySimpleGUI as psg
+from mpl_toolkits.mplot3d import axes3d
+#import PySimpleGUI as psg
 from scipy.optimize import least_squares
-
-
-
+from scipy.spatial.transform import Rotation as spr
+import logging
 from DroidControl import droidControl
 
 # Setup logging (Use droidlogging.conf as alternative)
@@ -67,43 +67,17 @@ class droidInertial(MPU6050):
         
         self.rawOmega = np.array([0,0,0])
         self.theta = np.array([0,0,0])
-        self.omegaBias = np.array([0,0,0])
+        self.gyroBias = np.array([0,0,0])
         
         self.imuOut = []        
-        debug_output = False
         i2c_bus = 1
         device_address = 0x68
 
         # Check for IMU bias file
-        try:
-            [x_accel_offset, y_accel_offset,
-              z_accel_offset, x_gyro_offset, y_gyro_offset, z_gyro_offset] = np.loadtxt('/home/pi/droidracer/MPUClass/imuBiases.csv', delimiter=',', dtype = "int")
-            logger.info('Initialising IMU biases to file.')
-            # Use existing IMU bias            
-            self.accelBias = np.array([x_accel_offset, y_accel_offset,z_accel_offset])
-            print(self.accelBias)
-            self.omegaBias = np.array([x_gyro_offset, y_gyro_offset, z_gyro_offset])
-            print(self.omegaBias)
-        
-        except:
-            [x_accel_offset, y_accel_offset,
-              z_accel_offset, x_gyro_offset, y_gyro_offset, z_gyro_offset] = np.array([0,0,0,0,0,0]) # Placeholder, run cal routine
-            logger.warning('No Bias file found. Static Calibration required')
+        self.loadCalData(loadBiasData =True)
 
-        # Check for IMU Offset file (Not yet implemented)
-        try:
-            self.imuOffset = np.loadtxt('/home/pi/droidracer/MPUClass/imuOffsets.csv', delimiter=',', dtype = "int")
-            logger.info('Initialising IMU offsets to file.')
-        except:
-            self.imuOffset = np.array([0,0,0]) # Placeholder, run cal routine
-            logger.warning('No Offset file found. Dynamic calibration required')
-             
-        # Setup thread safe timer
-        
         # Initialise device using inheritance
-        MPU6050.__init__(self, i2c_bus, device_address, x_accel_offset, y_accel_offset,
-              z_accel_offset, x_gyro_offset, y_gyro_offset, z_gyro_offset,
-              debug_output)
+        MPU6050.__init__(self, i2c_bus, device_address)
         
         # Initialise DMP
         self.dmp_initialize()
@@ -115,9 +89,9 @@ class droidInertial(MPU6050):
         logger.debug('FIFO Packet size: %s ' % self.FIFO_packet_size)
         logger.debug(('FIFO count: %s ' % hex(self.get_FIFO_count())))
         
-        self.set_x_accel_offset(x_accel_offset)
-        self.set_y_accel_offset(y_accel_offset)
-        self.set_z_accel_offset(z_accel_offset)
+#         self.set_x_accel_offset(x_accel_offset)
+#         self.set_y_accel_offset(y_accel_offset)
+#         self.set_z_accel_offset(z_accel_offset)
                
     def readIMUraw(self):
         self.TimeKminus1 = self.TimeK
@@ -125,7 +99,7 @@ class droidInertial(MPU6050):
         self.rawOmegaMinus1 = self.rawOmega
         self.TimeK = time.time()
         # : Store as numpy arrays as we will be using matrix operations for Kalman filter
-        self.rawAccel = np.array([self.get_acceleration()])/ 16384.0  * self.gravity
+        self.rawAccel = np.array([self.get_acceleration()])/ 2**14  * self.gravity
         self.rawOmega = np.array([self.get_rotation()]) / 131.0
                
     def readDMP(self):
@@ -168,9 +142,14 @@ class droidInertial(MPU6050):
     def storeIMUdata(self):
         temp = self.rawAccel.tolist() + self.rawOmega.tolist() +  [self.TimeK]
         self.imuOut.append(temp)
+        
     # TODO: Wrap calibration functions in seperate calibration class
-    def recordStaticIMU(self, sampleLength = 1000, saveData = True):
-        # GUI asks user to prepare droid for test n
+    def recordStaticIMU(self, sampleLength = 1000, saveData = True):       
+        # Ensure that existing biases are removed before calibration
+        self.set_x_accel_offset(0)
+        self.set_y_accel_offset(0)
+        self.set_z_accel_offset(0)         
+        
         poses = {'pose 1':(0,'all'),
                  'pose 2':(1,'1'),
                  'pose 3':(2,'1'),
@@ -179,7 +158,13 @@ class droidInertial(MPU6050):
                  'pose 6':(1,'3'),
                  'pose 7':(2,'3'),
                  'pose 8':(1,'1 & 2'),
-                 'pose 9':(2,'1 & 2'),}
+                 'pose 9':(2,'1 & 2'),
+                 'pose 10':('1 & 2','1 & 2'),
+                 'pose 11':('2 & 1','1 & 2'),
+                 'pose 12':('cushion','all'),
+                 'pose 13':('cushion','all'),
+                 'pose 14':('cushion','all'),
+                 'pose 15':('cushion','all'),}
         
         self.meanAccel=[]
         self.varAccel=[]
@@ -187,7 +172,7 @@ class droidInertial(MPU6050):
         
         # Get data at various poses
         for pose, blocks in poses.items():
-            print('\n','Set up robot in ',pose, 'with ',blocks[0], 'blocks under wheel ',blocks[1],'\n')
+            print('\n','Set up robot in',pose, 'with',blocks[0], 'blocks under wheel ',blocks[1],'\n')
             input('Press any key to continue, ctrl C to quit')
             # Get initial reading
             self.readIMUraw()
@@ -197,7 +182,8 @@ class droidInertial(MPU6050):
                 # Get raw IMU data
                 self.readIMUraw()
                 self.poseAccel = np.concatenate((self.poseAccel, self.rawAccel),axis=0)
-                print('.',)
+                print('.', end="", flush=True)
+                time.sleep(0.005)
                 
             
             calTime = time.time()-t00
@@ -215,26 +201,119 @@ class droidInertial(MPU6050):
         if saveData:
             np.savetxt('staticData.csv',self.accelData[1:],delimiter=',')
             logger.info('Saving static data to file: %s entries' % len(self.accelData))
-    
-    def calStatic(self, getStaticData = False):
+            
+    def loadCalData(self, loadBiasData = False, loadStaticData=False):
+        if loadStaticData is True:       
+            try:
+                self.accelData = np.loadtxt('/home/pi/droidracer/staticData.csv', delimiter=',', dtype = "float")
+                logger.info('staticData.csv loaded')
+            except:
+                logger.info('staticData.csv file not found')
+                userIO = input('\n Press y to perform static calibration or any other key to Quit \n')
+                if userIO == 'y':
+                    self.recordStaticIMU()
+                else:
+                    logger.info('Quitting script')
+                    exit()
+        else:
+            pass
+        
+        if loadBiasData is True:       
+            try:
+                IMUbiases = np.loadtxt('/home/pi/droidracer/imuBiases.csv', delimiter=',', dtype = "int")
+                logger.info('Initialising IMU biases to file.')
+                # Use existing IMU bias            
+                self.accelBias = np.array(IMUbiases[0:3])
+                self.gyroBias = np.array(IMUbiases[3:6])
+            
+            except:
+                self.accelBias = np.array([0,0,0])
+                self.gyroBias = np.array([0,0,0])
+                logger.warning('No Bias file found. Static Calibration required')
 
-        try:
-            self.accelData = np.loadtxt('/home/pi/droidracer/staticData.csv', delimiter=',', dtype = "float")
-            logger.info('staticData.csv loaded')
-        except:
-            logger.info('staticData.csv file not found, launching calibration routine')
-            self.recordStaticIMU()
+        else:
+            pass
+        # Check for IMU Offset file (Not yet implemented)
+#                     try:
+#                 self.imuOffset = np.loadtxt('/home/pi/droidracer/MPUClass/imuOffsets.csv', delimiter=',', dtype = "int")
+#                 logger.info('Initialising IMU offsets to file.')
+#             except:
+#                 self.imuOffset = np.array([0,0,0]) # Placeholder, run cal routine
+#                 logger.warning('No Offset file found. Dynamic calibration required')
+  
+    def calStatic(self, getStaticData = False, optType = 'norm', saveData = True):
+
+        
         if getStaticData is True:
             logger.info('Get ready to acquire static calibration data from robot!')
             self.recordStaticIMU()
+        else:
+            self.loadCalData(loadStaticData = True)
+                       
+        if optType is 'norm':
+            b0 = np.array([0,0,0])
+            obsA = self.accelData
+            grav = -self.gravity
+            self.biasEstimate = least_squares(self.costFunction, b0, method='trf',ftol=1e-13, verbose=2, args=(grav, obsA))
             
+        elif optType is 'rotation':
+            b0 = np.array([0,0,0])
+            obsA = self.accelData
+            grav = np.array([[0,0,-self.gravity]]* 900)
+            # Make call to non-lin least squares for parameter estimation
+            self.biasEstimate = least_squares(self.costFunction2, b0, method='trf',ftol=1e-13, verbose=2, args=(grav, obsA))
+        
+
+        logger.info('Bias estimation complete: ')
+        self.accelBias = self.biasEstimate.x * 16384.0  / self.gravity
+        logger.debug(self.accelBias)
+        
+        if saveData:
+            # TODO: This is hacky, consider gyro info seperately
+            saveFile = [self.accelBias[0],self.accelBias[1],self.accelBias[2],0,0,0]
+            np.savetxt('imuBiases.csv',saveFile,delimiter=',')
+            logger.info('Saving static accelerometer biases to file')
             
-        b0 = self.accelBias / 16384.0  * self.gravity
-        b0 = b0.tolist()
-        Ax = self.accelData[:,0].tolist()
-        self.biasEstimate = least_squares(costFunction, b0, args=(self.gravity, Ax))
-        logger.info('Bias estimation complete: ',self.biasEstimate.x)
-        logger.debug(self.biasEstimate.x * 16384.0  / self.gravity)
+    def costFunction(self, bias, grav, obs):
+                
+        Ax = obs[:,0]
+        Ay = obs[:,1]
+        Az = obs[:,2]
+        Bx = bias[0]
+        By = bias[1]
+        Bz = bias[2]
+        # Find residual : E = norm(g) - norm(A-B)
+        cost = self.gravity - np.sqrt((Ax - Bx)**2 + (Ay - By)**2 + (Az-Bz)**2)
+        return cost
+    
+    def costFunction2(self, bias, grav, obs):
+        #THIS needs much work, align vector attempts to find true rotation. This requires adjustment to bias
+        sampleLength = 1000
+        A = np.array([[0,0,0]])
+        B = np.array([[0,0,0]])
+        
+        for poseIdx in range(9):        
+            # Calculate closest rotation matrix using current estimate of bias
+            start = (poseIdx)*sampleLength
+            end = start + 1000
+            tempObs = obs[start:end,:]
+            C = spr.align_vectors(tempObs - bias, grav)
+            # Concatenate to variable for each pose
+            tempBias = obs[start:end,:] - C[0].apply(grav)
+            A = np.concatenate((A,tempObs),axis=0)
+            B = np.concatenate((B,tempBias),axis=0)
+            
+            # Get value of bias estimate that allows for proper rotation
+        
+        Ax = A[1:-1,0]
+        Ay = A[1:-1,1]
+        Az = A[1:-1,2]
+        Bx = B[1:-1,0]
+        By = B[1:-1,1]
+        Bz = B[1:-1,2]
+        # Find residual : E = norm(g) - norm(A-B)
+        cost = self.gravity - np.sqrt((Ax - Bx)**2 + (Ay - By)**2 + (Az-Bz)**2)
+        return cost
     
         
     def calFindOffsets (self,calTime = 5, calOmega = 0.5,saveData = False):
@@ -271,34 +350,52 @@ class droidInertial(MPU6050):
             
             
     def testReadSpeed(self):
-       print("Test read time - RAW")
-       counter = 0
-       t0 = time.time()
-       self.readIMUraw()
-       accelRaw = self.rawAccel
-       while counter < 100:
-           self.readIMUraw()
-           accelRaw = np.concatenate((accelRaw, self.rawAccel),axis=0)
-           time.sleep(0.008)
-           counter += 1
-       print('100 Raw values in: %f' % (self.TimeK - t0))
-       print('Average read time: %f' % ((self.TimeK - t0) / 100))
+        print("Test read time - RAW")
+        counter = 0
+        t0 = time.time()
+        self.readIMUraw()
+        accelRaw = self.rawAccel
+        while counter < 100:
+            self.readIMUraw()
+            accelRaw = np.concatenate((accelRaw, self.rawAccel),axis=0)
+            time.sleep(0.008)
+            counter += 1
+        print('100 Raw values in: %f' % (self.TimeK - t0))
+        print('Average read time: %f' % ((self.TimeK - t0) / 100))
        
-       print("Test read time - DMP")
-       counter = 0
-       self.reset_FIFO()
-       t0 = time.time()
-       self.readDMP()
-       accelDmp = self.dmpAccel
-       while counter < 100:
+        print("Test read time - DMP")
+        counter = 0
+        self.reset_FIFO()
+        t0 = time.time()
+        self.readDMP()
+        accelDmp = self.dmpAccel
+        while counter < 100:
            self.readDMP()
            accelDmp = np.concatenate((accelDmp, self.dmpAccel),axis=0)
            counter += 1
-       print('100 DMP values read in: %f' % (self.TimeK - t0))
-       print('Average read time: %f' % ((self.TimeK - t0) / 100))
+        print('100 DMP values read in: %f' % (self.TimeK - t0))
+        print('Average read time: %f' % ((self.TimeK - t0) / 100))
+        
+    def plotDataSet(self,data):
+        
+        fig, ax = plt.subplots(1,3,figsize=(12,9))
+
+        ax[0,0].plot(data[:,0],'r',label='Accel - x (m/s^2)')
+        ax[0,0].set_ylabel('Accel - (m/s^2)')
+        ax[0,1].plot(data[:,1],'g',label='Accel - y (m/s^2)')
+        ax[0,2].plot(data[:,2],'b',label='Accel - z (m/s^2)')
+        fig.legend(loc="upper right", bbox_to_anchor=(1,1), bbox_transform=ax.transAxes)
+
+        plt.show()
        
     def testLinProp(self):
         print("Test linear propagation")
+        
+#         # Ensure that calibration biases are used
+#         self.set_x_accel_offset(self.accelBias[0])
+#         self.set_y_accel_offset(self.accelBias[1])
+#         self.set_z_accel_offset(self.accelBias[2])   
+#         
         # Warm up IMU
         for idx in range(10):
             self.readIMUraw()
@@ -307,7 +404,7 @@ class droidInertial(MPU6050):
             
         # Initialise storage containers   
         self.propagateLinear()
-        accelRaw = self.rawAccel
+        accelRaw = self.rawAccel - (self.accelBias/2**14 * self.gravity)
         dispRaw = self.displacement
         velRaw = self.velocity
         
@@ -347,23 +444,100 @@ class droidInertial(MPU6050):
         
         plt.show()
         
-def costFunction(bias, grav, obs):
-    return grav - np.sqrt((obs[0] - bias[0])**2 + (obs[1] - bias[1])**2 + (obs[2] - bias[2])**2)
-                
-                
+    def estPose(self,obs,bias,sampleLength=1000):
+        
+        # THIS NEEDS MUXCH WORK!!!!!!!!!
+        # Calculate robot pose using gravity vector from dataset, Add magnwtometer later  
+        Ax = obs[:,0]
+        Ay = obs[:,1]
+        Az = obs[:,2]
+        B = bias*self.gravity /2**14
+        grav = np.array([[0,0,-self.gravity]]*sampleLength)
+        rotOut=[] 
+#         A = np.array([[0,0,0]])
+        for poseIdx in range(9):        
+            # Calculate closest rotation matrix using current estimate of bias
+            startIdx = (poseIdx)*sampleLength
+            endIdx = startIdx + sampleLength
+            tempObs = obs[startIdx:endIdx,:]
+            rotObj = spr.align_vectors(tempObs - B, grav)
+            # Concatenate to variable for each pose
+#             rotation = C[0].as_euler('zyx',degrees=True)
+#             A = np.concatenate((A,rotation),axis=0)
+            rotOut.append(rotObj[0])    
+        return rotOut
+    
+    def plotPose(self,pose, poseNo):
+        fig = plt.figure(figsize=(9,9))
+        ax = fig.gca(projection='3d')
+        
+        #Set origin of world frame
+        x0 = [0,0,0]
+        y0 = [0,0,0]
+        z0 = [0,0,0]
+        
+        # Create world frame arrows      
+        u0 = [1,0,0]
+        v0 = [0,1,0]
+        w0 = [0,0,1]
+        
+        colours = ['r','g','b']
+        
+        plotScale = 10
+        ax.quiver(x0,y0,z0,u0,v0,w0,color=[[0.7,0.7,0.7]]*3,length = plotScale,arrow_length_ratio=0.1)
+        ax.text(plotScale,0,0.1,r'$X_w$')
+        ax.text(0,plotScale,0,r'$Y_w$')
+        ax.text(0,0,plotScale,r'$Z_w$')
+        
+        # Create gravity vector
+        ax.quiver(0,0,0,0,0,-9.8,color='r',arrow_length_ratio=0.2)
+        ax.text(0,0,-10,'Gravity',color ='r')
+        
+        # Create rotated coordinate system
+        uR, vR, wR = pose.apply([u0,v0,w0])
+        ax.quiver(x0,y0,z0,uR,vR,wR,color=colours,length = 8,arrow_length_ratio=0.1)
+        ax.text(8,0,0.1,r'$X_r$',color ='r')
+        ax.text(0,8,0,r'$Y_r$',color ='g')
+        ax.text(0,0,8,r'$Z_r$',color ='b')
+        
+        ax.set_xlim3d(-10,10)
+        ax.set_ylim3d(-10,10)
+        ax.set_zlim3d(-10,10)
+        
+        ax.set_xlabel(r'$A_x - ms^(-1)$')
+        ax.set_ylabel(r'$A_y - ms^(-1)$')
+        ax.set_zlabel(r'$A_z - ms^(-1)$')
+        
+        Euler = pose.as_euler('zyx',degrees=True)        
+        ax.set_title(r'Pose: {0} $R_x$: {1:.2f} $R_y$: {2:.2f} $R_y$: {3:.2f}'.format(poseNo, Euler[0], Euler[1], Euler[2]))
+
+    def checkRotation(self,R):
+        R = pose.as_matrix()
+        deter = np.round(np.linalg.det(R),decimals=4)
+        if deter == 1:
+            isRot = 'rotation'
+        elif deter == -1:
+            isRot = 'reflection'
+        else:
+            isRot = '??'
+        return isRot, deter
 
 if __name__ == "__main__":
     di = droidInertial()
 #     di.testReadSpeed()
+#     di.calStatic(getStaticData = False)
+#     di.plotDataSet(di.accelData)
 #     di.testLinProp()
-#     di.recordStaticIMU()
-    di.calStatic(getStaticData = False)
-    
-   
-    
-    
-    
-    
-
-    
+    di.loadCalData(loadStaticData = True)
+    rotations = di.estPose(di.accelData, di.accelBias)
+    for poseNo,pose in enumerate(rotations,start=1):
+        #di.plotPose(pose,poseNo)
+        isRot, deter = di.checkRotation(pose)
+        print('Pose {0} is a {1}, determinant ={2:.2f}'.format(poseNo,isRot, deter))
         
+        
+    plt.show()
+        
+        
+        
+         
